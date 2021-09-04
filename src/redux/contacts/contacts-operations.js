4b8dd33f7a61cd485041998fd5eeda41c66a3398
Iryna Volosozharova <iradevis@gmail.com>
2021-09-05 00:14:22 +0300

    fix base settings

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as actions from "./contacts-actions";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://connections-api.herokuapp.com";
 
 export const fetchContacts = () => (dispatch) => {
   dispatch(actions.fetchContactRequest());
